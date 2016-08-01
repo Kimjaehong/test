b80cf871dc87de3e87268d4f375e165ad25b0126
Kimjaehong <kingjhong@hanmail.net>
2016-08-01 16:45:30 +0900

    비밀번호 불일치 에러

--- src/main/java/com/issueking/test/api/persistance/user/UserMapper.java
+++ src/main/java/com/issueking/test/api/persistance/user/UserMapper.java
@@ -3,13 +3,15 @@ package com.issueking.test.api.persistance.user;
 import java.util.Collection;
 
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import com.issueking.test.api.bean.user.CustomUserDetails;
 import com.issueking.test.api.util.Role;
 
+@Repository(value="UserMapper")
 public interface UserMapper {
     
-    @Select("SELECT * FROM users WHERE username = #{username}")
+    //@Select("SELECT * FROM users WHERE username = #{username}")
     public CustomUserDetails getUser(String username);
     
     @Select("SELECT * FROM authorities WHERE username = #{username}")
